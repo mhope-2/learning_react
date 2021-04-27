fd6cfbce240779579d3e85a5dd27b82ec25ba892
mhope <mhope@npontu.com>
2021-04-27 07:10:16 +0000

    feat(api fetch): added axios for api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import {ContactCard, ContactCardProps} from "./components/contactcard/ContactCard"
 import contactData from './data/contactCardData'
 import './App.css';
+import axios from 'axios';
+
 
 type ApplicationState = {
-  contactComponentData: ContactCardProps[] //contact card
+  contacts: ContactCardProps[], //contact card
+  isLoading: boolean,
+  character: any
 };
 
 
@@ -13,31 +17,114 @@ class App extends React.Component<{}, ApplicationState>{
   constructor(props: any){
     super(props)
     this.state = {
-      contactComponentData: contactData
+      contacts: contactData,
+      isLoading: false,
+      character: {}
     }
   }
 
+
+  // Life Cycle methods
   componentDidMount(){
     document.title = "Index"
+
+    // change loading state to true before api fetch
+    this.setState({isLoading:true})
+
+    // GET request for remote image in node.js
+    axios({
+      method: 'GET',
+      url: 'https://swapi.dev/api/people/1',
+      responseType: 'json'
+    })
+    .then(res => {
+        this.setState({ 
+          character: res.data,
+          isLoading: false
+        })
+      })
+
+
+
+    // setTimeout(()=>{
+    //     this.setState({
+    //       isLoading: false
+    //     })
+    // }, 1500)
+
+
   }
+
+  componentDidUpdate(){
+     
+  }
+
+  // static getDerivedStateFromProps(props:any, state:any){
+  //   // return the new updated state based on the props
+
+  // }
+
+
+  // DEPRECATED 
+  // componentWillReceiveProps(nextProps:any){
+  //   // call if changes are to be made to an incoming props object
+  //   if (nextProps.option !== ''){ // check for changes in incoming props
+  //       // Take an action
+  //   } 
+  // }
+
+  // getSnapshotBeforeUpdate(){
+  //   // create a backup of the way things are
+  // }
+
+  shouldComponentUpdate(nextProps:any, nextState:any){
+    // return true if want to update; return false otherwise
+    return true
+
+  }
+
+  componentWillUnmount(){
+    // clean up or tear down before component disappears
+    // remove event listeners
+
+  }
+
+  // class methods
+  handleChange(id: number){
+    this.setState(previousState=>{
+      const updatedContacts = previousState.contacts.map(contact=>{
+          if (contact.id === id){
+              contact.name = "Changed Name"
+          }
+          return contact
+      })  
+      return {
+        contacts : updatedContacts
+      }
+    })
+
+    //binding
+    this.handleChange = this.handleChange.bind(this)
+  }
+
   
   render(){
 
     // Get contact data from an array
-    const contactDataItems = this.state.contactComponentData.map(contact => {
+    const contactDataItems = this.state.contacts.map(contact => {
       return <ContactCard  key={contact.id} id={contact.id} name={contact.name} 
                            imgUrl={contact.imgUrl} phone={contact.phone} 
                            email={contact.email}
                          />
-  })  
-
+  })   
     return (
       <div>
-        {contactDataItems}
-        
-     </div>
-  )
+        {this.state.isLoading ? <p>Loading...</p> : contactDataItems }
+        {this.state.isLoading ? <p>Loading...</p> : this.state.character.name}
+      </div>
+    )
   }
-}
+    
+  }
 
 export default App;
--- src/components/contactcard/ContactCard.tsx
+++ src/components/contactcard/ContactCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export interface ContactCardProps {  
     id: number,
     name: string,
@@ -10,7 +11,8 @@ export interface ContactCardProps {
 
 export interface ContactCardState {
   message: string,
-  isLoggedIn: boolean
+  isLoggedIn: boolean,
+  count: number
 }
 
 export class ContactCard  extends React.Component<ContactCardProps, ContactCardState> {
@@ -19,28 +21,38 @@ export class ContactCard  extends React.Component<ContactCardProps, ContactCardS
     super(props)
     this.state = {
       message: "I am from the state",
-      isLoggedIn: true
+      isLoggedIn: true,
+      count: 0,
     }
 
     // bind method to class (for setState())
     this.handleBtnClick = this.handleBtnClick.bind(this)
+    this.handleImgHover = this.handleImgHover.bind(this)
   }
 
+  
+
   // class methods
-  handleImgHover = ()=>console.log("Hovered")
-  handleBtnClick = () => this.setState({isLoggedIn:false})
+  handleImgHover = (id:number)=>{console.log(id)}
+
+  handleBtnClick = () => this.setState(previousState =>{
+        return {
+          count: previousState.count + 1
+        }        
+  })
 
     render(){
-
+ 
     return (
       <div className="contact-card">
-        <img onMouseOver={this.handleImgHover} src = {this.props.imgUrl} alt="" />
+        <img onMouseOver={() => this.handleImgHover(this.props.id)} src = {this.props.imgUrl} alt="" />
         <h3>Name: {this.props.name}</h3>
         <p>Phone: {this.props.phone}</p>
         <p>Email: {this.props.email}</p>
         <p>{this.state.isLoggedIn ? 'You are logged in': 'You are not logged in'}</p>
+        <p>{this.state.count}</p>
         <button onClick={this.handleBtnClick}>Change State</button><br/><br/>
-       </div>
+      </div>
     )
     }
   }
